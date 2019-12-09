6ed730c4bd13c933365a7e72a971ab35a7712697
Ross Mawdsley <rossmawd@gmail.com>
2019-12-09 23:57:55 +0000

    numbers rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -7,14 +6,58 @@ class App extends React.Component {
 
   }
 
+  componentDidMount() {
+    this.renderNumber()
+  }
 
+  styleHighlight = {
+    backgroundColor: "green"
+  }
+
+  toggleMultiples = () => {
+
+  }
+
+
+  renderNumber = (number) => {
+
+    console.log(number)
+    return (
+      <div
+        class="grid-item"
+        style={this.styleHighlight}
+        onClick={this.toggleMultiples}
+      >
+        {number}
+      </div>
+    )
+  }
 
 
   render() {
+    let numbers = Array.from(Array(144), (x, index) => x = index + 1)
+
     return (
       <div className="App">
-        <span> Lets get started</span>
-      </div>
+
+        <div class="grid-container">
+          {/* <div class="grid-item" style={this.styleX}>1</div> */}
+          {numbers.map(number => (
+            this.renderNumber(number)
+          ))
+
+
+          }
+
+          {/* {names.map(name => (
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))} */}
+
+
+        </div>
+      </div >
     );
   }
 }
