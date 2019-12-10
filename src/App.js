02da3cbf08470d996cd9c59f1588aa0e14aee0ce
Ross Mawdsley <rossmawd@gmail.com>
2019-12-10 01:16:38 +0000

    constructing toggleMultiples function

--- src/App.js
+++ src/App.js
@@ -3,57 +3,77 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-
+    numberStates: null
   }
 
   componentDidMount() {
-    this.renderNumber()
+    this.constructNumbersArray()
+    console.log("The App Has mounted")
   }
 
-  styleHighlight = {
-    backgroundColor: "green"
+  constructNumbersArray = () => {
+    let numberArray = Array.from(Array(144), (x, index) => x = index + 1)
+    let arrayOfNumberObjects = []
+
+    numberArray.forEach(number => {
+      let numberObject = {}
+      numberObject[number] = false
+      arrayOfNumberObjects.push(numberObject)
+    })
+
+    this.setState({ numberStates: arrayOfNumberObjects }, () => console.log("Numbers Constructed!", this.state.numberStates))
   }
 
-  toggleMultiples = () => {
+  styleHighlight = (bool) => {
+    if (bool) {
+      return ({ backgroundColor: "green" })
+    }
+  }
+
+  toggleMultiples = (numberObj, i) => {
+    let currentFactor = i + 1
+    let numberStates = Object.assign([], this.state.numberStates)
+
+    if (numberObj[i + 1]) {
+      console.log(`Un-highlighting multiples of ${i + 1} now`)
+
+
+    } else {
+      console.log(`Highlighting multiples of ${i + 1} now`)
+    }
 
   }
 
 
-  renderNumber = (number) => {
-
-    console.log(number)
+  returnNumberJSX = (numberObj, i) => {
+    console.log(numberObj)
     return (
       <div
-        class="grid-item"
-        style={this.styleHighlight}
-        onClick={this.toggleMultiples}
+        className="grid-item"
+        style={this.styleHighlight(numberObj[i + 1])}
+        onClick={() => this.toggleMultiples(numberObj, i)}
+        key={i + 1}
       >
-        {number}
+        {i + 1}
       </div>
     )
+
   }
 
-
   render() {
-    let numbers = Array.from(Array(144), (x, index) => x = index + 1)
+    // let numbers = Array.from(Array(144), (x, index) => x = index + 1)
+    let numbers = Object.assign([], this.state.numberStates)
+    console.log(numbers)
 
     return (
       <div className="App">
 
-        <div class="grid-container">
+        <div className="grid-container">
           {/* <div class="grid-item" style={this.styleX}>1</div> */}
-          {numbers.map(number => (
-            this.renderNumber(number)
+          {numbers.map((number, i) => (
+            this.returnNumberJSX(number, i)
           ))
-
-
           }
-
-          {/* {names.map(name => (
-                  <option key={name} value={name}>
-                    {name}
-                  </option>
-                ))} */}
 
 
         </div>
