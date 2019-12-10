13f7e24da5d750665a4bfd8ac79cacc970d32b0f
Ross Mawdsley <rossmawd@gmail.com>
2019-12-10 15:18:48 +0000

    Prime hightlight now working

--- src/App.js
+++ src/App.js
@@ -3,63 +3,42 @@ import './App.css';
 
 class App extends React.Component {
   state = {
-    numberStates: null
+    numberStates: Array(144).fill(false)
   }
 
-  componentDidMount() {
-    this.constructNumbersArray()
-    // console.log("The App Has mounted")
-  }
-
-  constructNumbersArray = () => {
-    let numberArray = Array.from(Array(144), (x, index) => x = index + 1)
-    let arrayOfNumberObjects = []
-
-    numberArray.forEach(number => {
-      let numberObject = {}
-      numberObject[number] = false
-      arrayOfNumberObjects.push(numberObject)
-    })
-
-    this.setState({ numberStates: arrayOfNumberObjects }, () => console.log("Numbers Constructed!", this.state.numberStates))
-  }
-
-  styleHighlight = (bool) => {
-    if (bool) {
-      return ({ backgroundColor: "green" })
-    }
-  }
-
-  toggleMultiples = (factorObj, i) => {
+  toggleMultiples = (state, i) => {
     let currentFactor = i + 1
-    let currentFactorHighlighted = factorObj[i + 1]
     let numberStates = Object.assign([], this.state.numberStates)
 
-    numberStates = numberStates.map((numberObj, i) => {
-
-      if (currentFactorHighlighted && (i + 1) % currentFactor === 0) {
-        numberObj = {}
-        numberObj[i + 1] = false
-      } else if (!currentFactorHighlighted && (i + 1) % currentFactor === 0) {
-        numberObj = {}
-        numberObj[i + 1] = true
+    numberStates = numberStates.map((x, i) => {
+      if (x && (i + 1) % currentFactor === 0) {
+        x = false
+      } else if (!x && (i + 1) % currentFactor === 0) {
+        x = true
       }
-      return numberObj
+      return x
     })
     this.setState({ numberStates: numberStates })
   }
 
-  returnNumberJSX = (numberObj, i) => {
-    return (
-      <div
-        className="grid-item"
-        style={this.styleHighlight(numberObj[i + 1])}
-        onClick={() => this.toggleMultiples(numberObj, i)}
-        key={i + 1}
-      >
-        {i + 1}
-      </div>
-    )
+  handleClick = () => {
+    this.setState({ numberStates: Array(144).fill(false) })
+  }
+
+  calculateHighlight = (state, i) => {
+    if (state) {
+      return "highlighted"
+    } else {
+      return this.isPrime(i + 1)
+    }
+  }
+
+  isPrime = (num) => {
+    if (num === 1) { return "grid-item" }
+    for (var i = 2; i < num; i++) {
+      if (num % i === 0) { return "grid-item"; }
+    }
+    return "prime"
   }
 
   render() {
@@ -67,9 +46,18 @@ class App extends React.Component {
 
     return (
       <div className="App">
+        <label> Green: factors || Blue: Prime </label>
+        <br />
+        <button onClick={() => this.handleClick()}> CLEAR ALL</button>
         <div className="grid-container">
-          {numberStates.map((numberObj, i) => (
-            this.returnNumberJSX(numberObj, i)
+          {numberStates.map((state, i) => (
+            <div
+              className={this.calculateHighlight(state, i)}
+              onClick={() => this.toggleMultiples(state, i)}
+              key={i + 1}
+            >
+              {i + 1}
+            </div>
           ))
           }
         </div>
@@ -83,8 +71,22 @@ export default App;
 
 
 
+// numberStates = numberStates.map((numberObj, i) => {
 
+//   if (currentFactorHighlighted && (i + 1) % currentFactor === 0) {
+//     numberObj = {}
+//     numberObj[i + 1] = false
+//   } else if (!currentFactorHighlighted && (i + 1) % currentFactor === 0) {
+//     numberObj = {}
+//     numberObj[i + 1] = true
+//   }
+//   return numberObj
+// })
 
 // let log = currentFactorHighlighted ? `Un-highlighting multiples of ${i + 1} now`
 // : `Highlighting multiples of ${i + 1} now`
 // console.log(log)
+
+  // numberStates = numberStates.filter((numberObj, i) => {
+    //   return numberObj[i + 1] % currentFactor === 0
+    // })
